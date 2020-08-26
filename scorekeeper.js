6f798d48135fd8bd7b00d68cbded590c5c890750
Jimmy Chancellor <jimbonesjim@gmail.com>
2020-08-25 23:27:57 -0600

    New localStorage System
    Change to objects in localStorage

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,75 +1,78 @@
+var left = {
+    name: "Mountain Bluebirds",
+    color: "blue",
+    score: 0,
+    things: 0
+};
+
+var right = {
+    name: "Boise Spuds",
+    color: "red",
+    score: 0,
+    things: 0
+};
+
+var fans = {
+    name: "Loyal Fans",
+    score: 0,
+    toggle: 't'
+};
+
 //Loads or makes the local storage
 window.onload = function() {
-    if(localStorage.bluescore&&localStorage.redscore&&localStorage.bluename&&localStorage.redname&&localStorage.lfscore&&localStorage.btscore&&localStorage.rtscore&&localStorage.lfname&&localStorage.lftoggle){
-	    document.getElementById("bluescore").innerHTML = localStorage.bluescore;
-		document.getElementById("redscore").innerHTML = localStorage.redscore;
-		document.getElementById("bluename").innerHTML = localStorage.bluename;
-		document.getElementById("redname").innerHTML = localStorage.redname;
-		document.getElementById("lfname").innerHTML = localStorage.lfname;
-		document.getElementById("lfscore").innerHTML = localStorage.lfscore;
-		document.getElementById("btscore").innerHTML = localStorage.btscore;
-		document.getElementById("rtscore").innerHTML = localStorage.rtscore;
-		somethingLF();
+    if (localStorage.getItem("leftSave") && localStorage.getItem("rightSave") && localStorage.getItem("fanSave")) {
+        left = JSON.parse(localStorage.getItem("leftSave"));
+        right = JSON.parse(localStorage.getItem("rightSave"));
+        fans = JSON.parse(localStorage.getItem("fanSave"));
 		console.log("Loaded saved variables from localStorage.")
-	} else {
-	    localStorage.bluename = "Mountain Bluebirds";
-	    localStorage.redname = "Boise Spuds";
-		localStorage.lfname = "Loyal Fans";
-        localStorage.bluescore = 0;
-	    localStorage.redscore = 0;
-		localStorage.lfscore = 0;
-		localStorage.btscore = 0;
-		localStorage.rtscore = 0;
-		localStorage.lftoggle = 't';
-		localStorage.lcolor = "blue";
-		localStorage.rcolor = "red";
-	    document.getElementById("bluescore").innerHTML = localStorage.bluescore;
-		document.getElementById("redscore").innerHTML = localStorage.redscore;
-		document.getElementById("lfname").innerHTML = localStorage.lfname;
-		document.getElementById("lfscore").innerHTML = localStorage.lfscore;
-		document.getElementById("btscore").innerHTML = localStorage.btscore;
-		document.getElementById("rtscore").innerHTML = localStorage.rtscore;
-		document.getElementById("bluename").innerHTML = localStorage.bluename;
-		document.getElementById("redname").innerHTML = localStorage.redname;
-		console.log("Created new variables into localStorage.");
-	}
+    }
+    document.getElementById("bluescore").innerHTML = left.score;
+    document.getElementById("redscore").innerHTML = right.score;
+    document.getElementById("bluename").innerHTML = left.name;
+    document.getElementById("redname").innerHTML = right.name;
+    document.getElementById("lfname").innerHTML = fans.name;
+    document.getElementById("lfscore").innerHTML = fans.score;
+    document.getElementById("btscore").innerHTML = left.things;
+    document.getElementById("rtscore").innerHTML = right.things;
+    somethingLF();
+    save();
 	KEEPBUTTONSBLACK();
-	changeColor("Left", localStorage.lcolor);
-	changeColor("Right", localStorage.rcolor);
+	changeColor("Left", left.color);
+	changeColor("Right", right.color);
 	console.log("Finished starting.");
 	}
 
+function save() {
+    localStorage.setItem('leftSave', JSON.stringify(left));
+    localStorage.setItem('rightSave', JSON.stringify(right));
+    localStorage.setItem('fanSave', JSON.stringify(fans));
+    console.log("saved");
+}
+
 //adds and removes points to teams
-function score(num, team) {	
-    if (team == "blue"){
-       if(num == "?"){
-	      var n = prompt("points to blue team", "10");
-		  if (isNaN(n)|| n === ""|| n == " "||n == null){
+function score(num, team) {
+    if (num == "?") {
+        var n = prompt("points to " + team + " team", "10");
+        if (isNaN(n)|| n === ""|| n == " "||n == null){
 			return;
-		  }
-		  num = parseInt(n);
-	   }
-	   localStorage.bluescore = Number(localStorage.bluescore) + num;
-	   document.getElementById("bluescore").innerHTML = localStorage.bluescore;
-   } else if(team == "red"){
-       if(num == "?"){
-	       var n = prompt("points to red team", "10");
-		   if (isNaN(n)|| n === ""|| n == " "||n == null){
-		   return;
-		   }
-		   num = parseInt(n);
-		}
-		localStorage.redscore = Number(localStorage.redscore) + num;
-	    document.getElementById("redscore").innerHTML = localStorage.redscore;
-   } else if(team == "lf"){
-       localStorage.lfscore = Number(localStorage.lfscore) + num;
-	   document.getElementById("lfscore").innerHTML = localStorage.lfscore;
-   } else if(team == "bt"){
-     localStorage.btscore = Number(localStorage.btscore) + num;
-	 document.getElementById("btscore").innerHTML = localStorage.btscore;
+        }
+        num = parseInt(n);
+    }
+    if (team == "left"){
+	   left.score = Number(left.score) + num;
+	   document.getElementById("bluescore").innerHTML = left.score;
+   } else if(team == "right"){
+		right.score = Number(right.score) + num;
+	    document.getElementById("redscore").innerHTML = right.score;
+   } else if(team == "fans"){
+      fans.score = Number(fans.score) + num;
+	   document.getElementById("lfscore").innerHTML = fans.score;
+   } else if(team == "lt"){
+     left.things = Number(left.things) + num;
+	 document.getElementById("btscore").innerHTML = left.things;
    } else if(team == "rt"){
-     localStorage.rtscore = Number(localStorage.rtscore) + num;
-	 document.getElementById("rtscore").innerHTML = localStorage.rtscore;
+     right.things = Number(right.things) + num;
+	 document.getElementById("rtscore").innerHTML = right.things;
    } else {
      alert("An error occurred while trying to add a score.");
 	 console.log("An error occurred while trying to add a score.");
@@ -79,21 +82,25 @@ function score(num, team) {
 	   console.log("removed " + Math.abs(num) + " from the " + team + " team");
    } else if (num > 0){
 	   console.log("added " + num + " to the " + team + " team");
-   }}
+   }
+    save();
+}
    
  //reset the scores for the teams
 function resetScore(team){
     if(confirm("Do you really want to reset the score for the " + team +" team")){
-	 if(team == "blue"){
-	    localStorage.bluescore = 0;
-		document.getElementById("bluescore").innerHTML = localStorage.bluescore;
-		localStorage.btscore = 0;
-		document.getElementById("btscore").innerHTML = localStorage.btscore;
-	} else if (team == "red"){
-	    localStorage.redscore = 0;
-		document.getElementById("redscore").innerHTML = localStorage.redscore;
-		localStorage.rtscore = 0;
-		document.getElementById("rtscore").innerHTML = localStorage.rtscore;
+	 if(team == "left"){
+	    left.score = 0;
+		document.getElementById("bluescore").innerHTML = left.score;
+		left.things = 0;
+		document.getElementById("btscore").innerHTML = left.things;
+         save();
+	} else if (team == "right"){
+	    right.score = 0;
+		document.getElementById("redscore").innerHTML = right.score;
+		right.things = 0;
+		document.getElementById("rtscore").innerHTML = right.things;
+        save();
 	} else if (team = "all"){
 	    localStorage.clear();
 		location.reload();
@@ -101,7 +108,7 @@ function resetScore(team){
 	return;
 	}
 	console.log("reset the " + team + " team's score and name.");
-	}
+}
 	
 //Toggle the Loyal Fan div to hide or show
 function toggleLF(){
@@ -110,19 +117,20 @@ function toggleLF(){
 	if (x.style.display === 'none') {
 		x.style.display = 'block';
 		z.innerHTML = "Hide";
-		localStorage.lftoggle = 't';
+		fans.toggle = 't';
 	} else {
 		x.style.display = 'none';
 		z.innerHTML = "Show";
-		localStorage.lftoggle = 'f';
+		fans.toggle = 'f';
 	}
 	console.log("Toggled showing Loyal Fan area.");
+    save();
 	}
 	
 function somethingLF(){
 	var x = document.getElementById('loyalfans');
 	var z = document.getElementById('togglelf');
-	if (localStorage.lftoggle == 'f'){
+	if (fans.toggle == 'f'){
 		x.style.display = 'none';
 		z.innerHTML = "Show";
 	}
@@ -136,69 +144,68 @@ function switchShow(){
 	var r6 = document.getElementById('R6');
 	var b = document.getElementById('otherthings');
 	if (bt.style.display === 'none' && rt.style.display === 'none'){
-		b6.style.display = 'none';
-		r6.style.display = 'none';
-		bt.style.display ='block';
-		rt.style.display = 'block';
+		b6.style.display  = r6.style.display = 'none';
+		bt.style.display = rt.style.display ='block';
 		b.innerHTML = "6 Things";
 	} else {
-		b6.style.display =  'block';
-		r6.style.display =  'block';
-		bt.style.display = 'none';
-		rt.style.display = 'none';
+		b6.style.display = r6.style.display =  'block';
+		bt.style.display = rt.style.display = 'none';
 		b.innerHTML = "Other Things";
 	}
 	console.log("Toggled between showing the 6 things or other things scoring.");
-	}
+    save();
+}
 
 //changes the name for the teams
 function changeName(team) {
-    if(team == "blue"){
-	var btn = prompt("Set Blue Team Name", "Mountain Bluebirds");
+    if(team == "left"){
+	var btn = prompt("Set left Team Name", "Mountain Bluebirds");
 	if (btn == null){
 	return;
 	}
 	if(btn == ""){
-	localStorage.bluename = "Blue";
+	left.name = "Left";
 	} else {
-	localStorage.bluename = btn;
+	left.name = btn;
 	}
-	document.getElementById("bluename").innerHTML = localStorage.bluename;
-	console.log("Changed the " + team + " name to " + localStorage.bluename);
-	} else if(team == "red"){
-	var rtn = prompt("Set Red Team Name", "Boise Spuds");
+	document.getElementById("bluename").innerHTML = left.name;
+	console.log("Changed the " + team + " name to " + left.name);
+	} else if(team == "right"){
+	var rtn = prompt("Set right Team Name", "Boise Spuds");
 	if (rtn == null){
 	return;
 	}
 	if(rtn == ""){
-	localStorage.redname = "Red";
+	right.name = "Right";
 	} else {
-	localStorage.redname = rtn;
+	right.name = rtn;
 	}
-	document.getElementById("redname").innerHTML = localStorage.redname;
-	console.log("Changed the " + team + " name to " + localStorage.redname);
-	} else if(team == "lf"){
-	var lftn = prompt("Set Purple Team Name", "Loyal Fans");
+	document.getElementById("redname").innerHTML = right.name;
+	console.log("Changed the " + team + " name to " + right.name);
+	} else if(team == "fans"){
+	var lftn = prompt("Set fans Team Name", "Loyal Fans");
 	if (lftn == null){
 	return;
 	}
 	if(lftn == ""){
-	localStorage.lfname = "Purple";
+	fans.name = "Fans";
 	} else {
-	localStorage.lfname = lftn;
+	fans.name = lftn;
 	}
-	document.getElementById("lfname").innerHTML = localStorage.lfname;
-	console.log("Changed the " + team + " name to " + localStorage.lfname);
-	}}
+	document.getElementById("lfname").innerHTML = fans.name;
+	console.log("Changed the " + team + " name to " + fans.name);
+	}
+    save();
+}
 	
 //changes the color for the teams
 function changeColor(team, color) {
-	if (team == "Left") {
+	if (team == "left") {
 		if (color == '?'){
 		color = prompt("What Color would you like the " + team + " team to be", "blue");
-		localStorage.lcolor = color;
+		left.color = color;
 		} else {
-			color = localStorage.lcolor;
+			color = left.color;
 		}
 		if (color == "White" || color == "white") {
 			document.getElementById("BlueTeam").style.color = "Black";
@@ -207,12 +214,12 @@ function changeColor(team, color) {
 			KEEPBUTTONSBLACK()
 		}
 		document.getElementById("BlueTeam").style.backgroundColor = color;
-	} else if (team == "Right") {
+	} else if (team == "right") {
 		if (color == '?'){
 		color = prompt("What Color would you like the " + team + " team to be", "red");
-		localStorage.rcolor = color;
+		right.color = color;
 		} else {
-			color = localStorage.rcolor;
+			color = right.color;
 		}
 		if (color == "White" || color == "white") {
 			document.getElementById("RedTeam").style.color = "Black";
@@ -223,6 +230,7 @@ function changeColor(team, color) {
 		document.getElementById("RedTeam").style.backgroundColor = color;
 	}
 	console.log("Changed the " + team + " team color to " + color);
+    save();
 }
 
 function KEEPBUTTONSBLACK(){
